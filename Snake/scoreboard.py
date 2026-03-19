284d6c1666fb2064b71e688bac4c7cea9c048892
Alejandro Sánchez <alejandrosanchezlirola@gmail.com>
2026-03-19 11:27:22 +0100

    Snake: agrega reinicio de partida y guardado de high score

--- Snake/scoreboard.py
+++ Snake/scoreboard.py
@@ -8,6 +8,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("data.txt") as data:
+            self.highscore = int(data.read())
         self.color("white")
         self.penup()
         self.goto(0, 260)
@@ -16,7 +18,15 @@ class Scoreboard(Turtle):
 
     def update_scoreboard(self):
         self.clear()
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.write(f"Score: {self.score}  High Score: {self.highscore}", align=ALIGNMENT, font=FONT)
+
+    def reset(self):
+        if self.score > self.highscore:
+            self.highscore = self.score
+            with open("data.txt", "w") as data:
+                data.write(f"{self.highscore}")
+        self.score = 0
+        self.update_scoreboard()
 
     def game_over(self):
         self.goto(0, 0)
