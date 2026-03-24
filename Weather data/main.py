691aebc0e0436af61a310ff8ceb7ddaec5b9810f
Alejandro Sánchez <alejandrosanchezlirola@gmail.com>
2026-03-24 23:22:47 +0100

    Agregar proyecto Weather data

---
+++ Weather data/main.py
@@ -0,0 +1,54 @@
+# with open("./Weather data/weather_data.csv") as file:
+#     data = data.readlines()
+#     print(data)
+
+
+# import csv
+
+# with open("./Weather data/weather_data.csv") as file:
+#     data = csv.reader(file)
+#     temperatures = []
+#     for row in data:
+#         if row[1] != "temp":
+#             temperatures.append(int(row[1]))
+
+
+import pandas
+
+data = pandas.read_csv("./Weather data/weather_data.csv")
+# print(type(data))
+# print(data["temp"])
+# print(type(data["temp"]))
+
+data_dict = data.to_dict()
+print(data_dict)
+
+temp_list = data["temp"].to_list()
+print(temp_list)
+
+print(data["temp"].mean())
+print(data["temp"].max())
+
+# Get data in columns
+print(data["condition"])
+print(data.condition)
+
+# Get data in rows
+print(data[data.day == "Monday"])
+print(data[data.temp == data.temp.max()])
+
+monday = data[data.day == "Monday"]
+print(monday.condition)
+
+monday_temp = monday.temp[0]
+monday_temp_F = monday_temp * 9/5 + 32
+print(monday_temp_F)
+
+# Create a dataframe from scratch
+data_dict = {
+    "students": ["Amy", "James", "Angela"],
+    "scores": [76, 56, 65]
+}
+
+data = pandas.DataFrame(data_dict)
+data.to_csv("./Weather data/new_data.csv")
