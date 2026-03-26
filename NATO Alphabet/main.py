673773c28ecd86f6ba1404cf97149d7d67067a91
Alejandro Sánchez <alejandrosanchezlirola@gmail.com>
2026-03-26 20:53:32 +0100

    NATO Alphabet: agrega conversor de alfabeto fonetico

---
+++ NATO Alphabet/main.py
@@ -0,0 +1,14 @@
+import pandas
+
+data = pandas.read_csv("./NATO Alphabet/nato_phonetic_alphabet.csv")
+
+nato_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+
+
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    output_list = [nato_dict[letter] for letter in word]
+    print(output_list)
+
+
+generate_phonetic()
