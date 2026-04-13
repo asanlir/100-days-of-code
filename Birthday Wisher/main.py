dffb4f1e487c5ae81a20f9d8c0311103f2ce4666
Alejandro Sánchez <alejandrosanchezlirola@gmail.com>
2026-04-13 12:58:53 +0200

    Birthday Wisher: completa automatizacion de envios de cumpleaños

--- Birthday Wisher/main.py
+++ Birthday Wisher/main.py
@@ -8,13 +8,29 @@ MY_EMAIL = ""
 MY_PASSWORD = ""
 
 
-
 # Check if today matches a birthday in the birthdays.csv
+today = dt.datetime.now()
+today_tuple = (today.month, today.day)
 
-# Pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
+data = pd.read_csv("birthdays.csv")
+birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
 
-# Send the letter to that person's email address.
+if today_tuple in birthdays_dict:
 
+    birthday_person = birthdays_dict[today_tuple]
+    file_path = f"./Birthday Wisher/letter_templates/letter_{random.randint(1,3)}.txt"
 
+    # Pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        contents = contents.replace("[NAME]", birthday_person["name"])
 
-
+    # Send the letter to that person's email address.
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=birthday_person["email"],
+            msg=f"Subject:Happy Birthday!\n\n{contents}"
+        )
