6511988719d1b92ecb73db455c40e579a712a14b
Alejandro Sánchez <alejandrosanchezlirola@gmail.com>
2026-04-13 12:39:04 +0200

    Birthday Wisher: agrega estructura inicial y plantillas de cartas

---
+++ Birthday Wisher/main.py
@@ -0,0 +1,20 @@
+import datetime as dt
+import pandas as pd
+import random
+import smtplib
+
+
+MY_EMAIL = ""
+MY_PASSWORD = ""
+
+
+
+# Check if today matches a birthday in the birthdays.csv
+
+# Pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
+
+# Send the letter to that person's email address.
+
+
+
+
