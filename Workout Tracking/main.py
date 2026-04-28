40934f30ea08fd5c753231721800455f57a8d6af
Alejandro Sánchez <alejandrosanchezlirola@gmail.com>
2026-04-28 19:28:12 +0200

    feat(workout-tracking): guarda ejercicios en hoja de cálculo via Sheety

--- Workout Tracking/main.py
+++ Workout Tracking/main.py
@@ -1,4 +1,6 @@
 import requests
+from datetime import datetime
+
 
 GENDER = "YOUR_GENDER"
 WEIGHT_KG = "YOUR_WEIGHT"
@@ -8,7 +10,10 @@ AGE = "YOUR_AGE"
 APP_ID = "YOUR_APP_ID"
 API_KEY = "YOUR_API_KEY"
 
+
 exercise_endpoint = "https://app.100daysofpython.dev/v1/nutrition/natural/exercise"
+sheet_endpoint = "YOUR_SHEETY_ENDPOINT"
+
 
 exercise_text = input("Tell me which exercises you did: ")
 
@@ -27,4 +32,21 @@ parameters = {
 
 response = requests.post(exercise_endpoint, json=parameters, headers=headers)
 result = response.json()
-print(result)
+
+today_date = datetime.now().strftime("%d/%m/%Y")
+now_time = datetime.now().strftime("%X")
+
+for exercise in result["exercises"]:
+    sheet_inputs = {
+        "workout": {
+            "date": today_date,
+            "time": now_time,
+            "exercise": exercise["name"].title(),
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"]
+        }
+    }
+
+    sheet_response = requests.post(sheet_endpoint, json=sheet_inputs)
+
+    print(sheet_response.text)
