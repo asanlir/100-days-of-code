aaf036e31ac1096b23dcc2e07dab906090e4b024
Alejandro Sánchez <alejandrosanchezlirola@gmail.com>
2026-04-29 19:35:31 +0200

    feat(flight-deals): configurar endpoint de Sheety y variables de entorno

--- Flight Deals/data_manager.py
+++ Flight Deals/data_manager.py
@@ -1,3 +1,27 @@
+import os
+import requests
+from requests.auth import HTTPBasicAuth
+from dotenv import load_dotenv
+
+
+# Load environment variables from .env file
+load_dotenv()
+
+SHEETY_PRICES_ENDPOINT = "YOUR ENDPOINT HERE"
+
+
 class DataManager:
-    #This class is responsible for talking to the Google Sheet.
-    pass
+
+    def __init__(self):
+        self._user = os.environ["SHEETY_USERNAME"]
+        self._password = os.environ["SHEETY_PASSWORD"]
+        self._authorization = HTTPBasicAuth(self._user, self._password)
+        self.destination_data = {}
+
+    def get_destination_data(self):
+        # Use the Sheety API to GET all the data in that sheet and print it out.
+        response = requests.get(url=SHEETY_PRICES_ENDPOINT, auth=self._authorization)
+        data = response.json()
+        self.destination_data = data["prices"]
+        # print(data)
+        return self.destination_data
