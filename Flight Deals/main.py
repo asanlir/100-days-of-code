ca49b11cbd4faec72635b5ad0f361885b80b52f8
Alejandro Sánchez <alejandrosanchezlirola@gmail.com>
2026-04-29 20:34:08 +0200

    feat(flight-deals): enviar alertas por email y personalizar mensajes según escalas

--- Flight Deals/main.py
+++ Flight Deals/main.py
@@ -61,15 +61,23 @@ for destination in sheet_data:
 
     # Send Notifications
     if cheapest_flight.price != "N/A" and cheapest_flight.price < destination["lowestPrice"]:
-        pprint(f"Lower price flight found to {destination['city']}!")
         data_manager.update_lowest_price(destination["id"], cheapest_flight.price)
-        # notification_manager.send_sms(
-        #     message_body=f"Low price alert! Only GBP {cheapest_flight.price} to fly "
-        #                 f"from {cheapest_flight.origin_airport} to {cheapest_flight.destination_airport}, "
-        #                 f"on {cheapest_flight.out_date} until {cheapest_flight.return_date}."
-        # )
-        notification_manager.send_whatsapp(
-            message_body=f"Low price alert! Only GBP {cheapest_flight.price} to fly "
-                    f"from {cheapest_flight.origin_airport} to {cheapest_flight.destination_airport}, "
+        # Customise the message depending on the number of stops
+        if cheapest_flight.stops == 0:
+            message = f"Low price alert! Only GBP {cheapest_flight.price} to fly direct "\
+                    f"from {cheapest_flight.origin_airport} to {cheapest_flight.destination_airport}, "\
                     f"on {cheapest_flight.out_date} until {cheapest_flight.return_date}."
-        )
+        else:
+            message = f"Low price alert! Only GBP {cheapest_flight.price} to fly "\
+                    f"from {cheapest_flight.origin_airport} to {cheapest_flight.destination_airport}, "\
+                    f"with {cheapest_flight.stops} stop(s) "\
+                    f"departing on {cheapest_flight.out_date} and returning on {cheapest_flight.return_date}."
+
+        print(f"Check your email. Lower price flight found to {destination['city']}!")
+
+        # notification_manager.send_sms(message_body=message)
+        # SMS not working? Try whatsapp instead.
+        notification_manager.send_whatsapp(message_body=message)
+
+        # Send emails to everyone on the list
+        notification_manager.send_emails(email_list=customer_email_list, email_body=message)
