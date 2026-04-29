ee5dabae238eb77b51f27559e2e103d68710d010
Alejandro Sánchez <alejandrosanchezlirola@gmail.com>
2026-04-29 19:56:34 +0200

    feat(flight-deals): implementar NotificationManager con SMS y WhatsApp, e iterar sobre todos los destinos

--- Flight Deals/main.py
+++ Flight Deals/main.py
@@ -4,6 +4,8 @@ from datetime import datetime, timedelta
 from data_manager import DataManager
 from flight_search import FlightSearch
 from flight_data import find_cheapest_flight
+from notification_manager import NotificationManager
+
 
 # Conserve requests and preserve free plan by caching the API responses
 requests_cache.install_cache(
@@ -15,31 +17,43 @@ requests_cache.install_cache(
 )
 
 
+# Setup the data manager, flight search and notification manager instances
 data_manager = DataManager()
 sheet_data = data_manager.get_destination_data()
-pprint(sheet_data)
+# pprint(sheet_data)
+flight_search = FlightSearch()
+# Create an instance of the NotificationManager
+notification_manager = NotificationManager()
 
-# Set the Dates
+
+# Set the Dates and Origin Airport
 tomorrow = datetime.now() + timedelta(days=1)
 six_month_from_today = datetime.now() + timedelta(days=(6 * 30))
+ORIGIN_CITY_IATA = "LHR"  # London Heathrow
 
-# Do a Flight Search
-flight_search = FlightSearch()
 
-flights = flight_search.check_flights(
-    origin_city_code="LHR",
-    destination_city_code="CDG",
-    from_time=tomorrow,
-    to_time=six_month_from_today
-)
+# Find Cheap Flights
+for destination in sheet_data:
+    pprint(f"Getting flights for {destination['city']}...")
+    flights = flight_search.check_flights(
+        ORIGIN_CITY_IATA,
+        destination["iataCode"],
+        from_time=tomorrow,
+        to_time=six_month_from_today
+    )
+    cheapest_flight = find_cheapest_flight(flights, return_date=six_month_from_today.strftime("%Y-%m-%d"))
+    pprint(f"{destination['city']}: GBP {cheapest_flight.price}")
 
-# pprint(flights)
-
-# Show the Cheapest Flight
-
-cheapest_flight = find_cheapest_flight(flights, return_date=six_month_from_today.strftime("%Y-%m-%d"))
-pprint(f"{sheet_data[0]['city']}: GBP {cheapest_flight.price}")
-
-if cheapest_flight.price != "N/A" and cheapest_flight.price < sheet_data[0]["lowestPrice"]:
-    pprint(f"Lower price flight found to {sheet_data[0]['city']}!")
-    data_manager.update_lowest_price(sheet_data[0]["id"], cheapest_flight.price)
+    if cheapest_flight.price != "N/A" and cheapest_flight.price < destination["lowestPrice"]:
+        pprint(f"Lower price flight found to {destination['city']}!")
+        data_manager.update_lowest_price(destination["id"], cheapest_flight.price)
+        # notification_manager.send_sms(
+        #     message_body=f"Low price alert! Only GBP {cheapest_flight.price} to fly "
+        #                 f"from {cheapest_flight.origin_airport} to {cheapest_flight.destination_airport}, "
+        #                 f"on {cheapest_flight.out_date} until {cheapest_flight.return_date}."
+        # )
+        notification_manager.send_whatsapp(
+            message_body=f"Low price alert! Only GBP {cheapest_flight.price} to fly "
+                    f"from {cheapest_flight.origin_airport} to {cheapest_flight.destination_airport}, "
+                    f"on {cheapest_flight.out_date} until {cheapest_flight.return_date}."
+        )
